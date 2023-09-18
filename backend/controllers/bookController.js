5bb184008b5912138bac8582193e05b128647411
rebeccaschou <rstchou@gmail.com>
2023-09-18 09:14:01 -0400

    Add GET, GET (all), and refactor POST

---
+++ backend/controllers/bookController.js
@@ -0,0 +1,48 @@
+const Book = require("../models/bookModel");
+const mongoose = require("mongoose");
+
+// GET all books
+const getBooks = async (req, res) => {
+  const books = await Book.find({}).sort({ createdAt: -1 });
+  res.status(200).json(books);
+};
+
+// GET a single book
+const getBook = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such book!" });
+  }
+
+  const book = await Book.findById(id);
+
+  if (!book) {
+    return res.status(404).json({ error: "No such book!" });
+  }
+
+  res.status(200).json(book);
+};
+
+// POST a new book
+const createBook = async (req, res) => {
+  const { title, author, description, rating } = req.body;
+
+  // add doc to db
+  try {
+    const book = await Book.create({ title, author, description, rating });
+    res.status(200).json(book);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// DELETE a new book
+
+// UPDATE a new book
+
+module.exports = {
+  getBooks,
+  getBook,
+  createBook,
+};
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,35 +1,27 @@
 const express = require("express");
+const {
+  getBooks,
+  getBook,
+  createBook,
+} = require("../controllers/bookController");
+
 const router = express.Router();
-const Book = require("../models/bookModel");
 
 // GET all books
-router.get("/", (req, res) => {
-  res.json({ message: "GET all books" });
-});
+router.get("/", getBooks);
 
 // GET a single book
-router.get("/:id", (req, res) => {
-  res.json({ message: "GET a single book" });
-});
+router.get("/:id", getBook);
 
 // POST a new book
-router.post("/", async (req, res) => {
-  const { title, author, description, rating } = req.body;
+router.post("/", createBook);
 
-  try {
-    const book = await Book.create({ title, author, description, rating });
-    res.status(200).json(book);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
-// DELETE a new book
+// DELETE a book
 router.delete("/:id", (req, res) => {
   res.json({ message: "DELETE a book" });
 });
 
-// UPDATE a new book
+// UPDATE a book
 router.patch("/:id", (req, res) => {
   res.json({ message: "UPDATE a book" });
 });
