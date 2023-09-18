e698390b5be0f84094355fa9554a13a7beab6c07
rebeccaschou <rstchou@gmail.com>
2023-09-18 00:10:46 -0400

    Add middleware

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,12 @@ const express = require("express");
 // express app
 const app = express();
 
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method); // prints requests to terminal
+  next();
+});
+
 // routes
 app.get("/", (req, res) => {
   res.json({ message: "Hello world!" });
