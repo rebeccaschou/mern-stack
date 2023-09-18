8be1ca55be27c122492d5097573040ef56bc5246
rebeccaschou <rstchou@gmail.com>
2023-09-18 18:01:52 -0400

    Add sorting feature

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,3 +1,4 @@
+// import { useEffect, useState } from "react";
 import { useEffect } from "react";
 import { useBooksContext } from "../hooks/useBooksContext";
 
@@ -7,6 +8,24 @@ import BookForm from "../components/BookForm";
 
 const Home = () => {
   const { books, dispatch } = useBooksContext();
+  // const [order, setOrder] = useState();
+
+  const changeOrder = (event) => {
+    // setOrder(event.target.value);
+    const sortedShelf = orderBooks(books, event.target.value);
+    dispatch({ type: "SET_BOOKS", payload: sortedShelf });
+  };
+
+  const orderBooks = (shelf, order) => {
+    switch (order) {
+      case "date":
+        return shelf.sort((a, b) => (a.createdAt > b.createdAt ? -1 : 1));
+      case "rating":
+        return shelf.sort((a, b) => (a.rating > b.rating ? -1 : 1));
+      default:
+        return shelf.sort((a, b) => (a.createdAt > b.createdAt ? -1 : 1));
+    }
+  };
 
   useEffect(() => {
     const fetchBooks = async () => {
@@ -23,6 +42,15 @@ const Home = () => {
 
   return (
     <div className="Home">
+      <div>
+        <label>
+          Sort Bookshelf by:
+          <select className="order-select" onChange={changeOrder}>
+            <option value="date">Date Added</option>
+            <option value="rating">Rating</option>
+          </select>
+        </label>
+      </div>
       <div className="books">
         {books &&
           books.map((book) => <BookDetails key={book._id} book={book} />)}
